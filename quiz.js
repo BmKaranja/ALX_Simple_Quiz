4383019351fef5904f4153a86d0599a3faaf18dd
Brian Karanja <bmkaranja001@gmail.com>
2025-10-05 10:18:49 +0300

    quiz-1

---
+++ quiz.js
@@ -0,0 +1,16 @@
+function checkAnswer(){
+    let checkAnswer='4'
+    document.querySelector.name('quiz')
+    let userAnswer = document.ATTRIBUTE_NODEquerySelector('input[name="answer"]:checked').value;
+    
+if (userAnswer === correctAnswer) {
+        document.getElementById('feedback').textContent="Correct Well done"
+    } else {
+        document.getElementById('feedback').textContent="That's incorrect. Try Again!"
+    }
+    
+};
+document.getElementById('submmit-answer')
+document.addEventListener;{
+    checkAnswer();
+};
